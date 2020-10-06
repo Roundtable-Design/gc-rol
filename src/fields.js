c6043d273a34f862329d1a8a62066e23359156cf
Gil <gilbertjvirgo@gmail.com>
2020-10-06 12:10:39 +0100

    Added Garden Church practices

--- src/fields.js
+++ src/fields.js
@@ -1,88 +1,109 @@
 const fields = [
 	{
-		label: "Daily Practices",
+		title: "Garden Practices",
 		practices: [
 			{
-				title:
-					"Firstly, what time of the day would you like to join the rest of KXC in reading scripture using the B.R.E.A.D tool?",
-				label: "Scripture",
+				title: "Prayer",
+				body:
+					"Live a life of Prayer and communion with God: (1) Abide in God’s love and in his presence; (2) Listen for how God speaks with you and invites you to Obey; and (3) Contend for the needs and for the renewal of your community, your city and the world. How will you spend this portion of your day? ",
 				value: "",
 				placeholder:
-					"Bread every morning followed by reading of Psalms",
+					"Type your answer here... e.g. Praying a Psalm, the Lord's Prayer, Breath Prayer, Listening Prayer, Intercession, etc.",
 			},
 			{
-				title:
-					"Next up, we're all looking to P.R.A.Y. in the morning and practice the Examen in the evening: what time are you going to aim to give each of these a go?",
-				label: "Prayer",
-				value: "",
+				title: "Scripture",
+				body:
+					"We long to be people who have soaked in words of Scripture.  When will you read and meditate on the Bible in order to orient your mind and heart around the living story of God's goodness and faithfulness?",
 				placeholder:
-					"Prayer in the morning, lord prayer’s midday, examen before bed",
+					"Type your answer here... e.g. Scripture reading, daily devotional, Lectio Divina, Bible Study, etc.",
+				value: "",
 			},
 			{
-				title:
-					"What practice is going to help your mind stay healthy throughout this time of social isolation?",
-				label: "Mind",
+				title: "Worship",
+				body:
+					"How will you prioritize your day to Worship God with gratitude and remember the moments of God's nearness and blessing during your day?",
 				value: "",
 				placeholder:
-					"Phone off at 9pm and on at 9am, don’t look at the news more than 2x",
+					"Type your answer here... e.g. worship through singing, keep a gratitude journal, Prayer of Examen, choose a few of Wesley's Noticing Questions to reflect on each evening",
 			},
 			{
-				title:
-					"Similarly, what practices around exercise and diet will help you care for your body?",
-				label: "Body",
-				value: "",
+				title: "Community",
+				body:
+					"We choose to make a covenantal commitment to a group of fellow Jesus followers and authentically share our lives with them in intimate Community. Who is your covenantal community (Digital Community, House church or others) and when do you meet with them to share life and grow together?",
 				placeholder:
-					"Walk everyday, exercise 3x a week, drink alchohol 3x a week",
+					"Type your answer here... e.g., Confession, Regular meals together, Share resources with each other, Prayer requests and prayer, etc",
+				value: "",
 			},
 			{
-				title:
-					"Fifthly, which spiritual discipline is going to help you stay soft-hearted and emotionally healthy?",
-				label: "Heart",
+				title: "Service",
+				body:
+					"We look beyond our own life and seek to be the hands and feet of Jesus in the world through sacrificial living and love of others. How will you commit to a life of Service for your family, neighbors, coworkers and your city?",
 				value: "",
 				placeholder:
-					"Read poetry at least three times a week and call Sam to chat",
+					"Type your answer here... e.g., Regular prayer walks; Sign up to serve at the rescue mission, Volunteer with our reach team; Invite neighbors over for a meal; Join alpha etc.",
 			},
 			{
-				title:
-					"And as a final daily practice, how are you going to think of others and love your neighbour?",
-				label: "Neighbour",
+				title: "Generosity",
+				body:
+					"As a community, we long to embody the extravagant Generosity that we see in Jesus. We cultivate this in us through a commitment and willingness to give and to bless others. How will you do this in your life?",
+				placeholder:
+					"Type your answer here... e.g., Monthly tithe to your church community, supporting missionaries or an organization you believe in, practice tipping 30%, regularly make or buy meals for those in need",
+				value: "",
+			},
+			{
+				title: "Steward",
+				body:
+					"We Steward our whole lives as a response to the grace and privilege God has given to each of us as his children. How will you be intentional with your  Time, Resources and Money and grow as a steward in God’s kingdom?",
 				value: "",
 				placeholder:
-					"Reach out to Rachel and Bruce and make sure they’re stocked up on food",
+					"Type your answer here... e.g., Commit to living on a budget and within your means, take an audit of your time and reorder how you prioritize your time and energy",
+			},
+			{
+				title: "Vocation",
+				body:
+					"Your Vocation is a missional space to steward and practice living as the way of Jesus. Who are people in your workplace you can pray for? How will you grow in your vocation and commit your work towards God’s kingdom purposes?",
+				placeholder:
+					"Type your answer here... e.g., Make a list of coworkers and commit to praying for them; invite Jesus into your work schedule and responsibilities to help navigate your vocation; Commit to weekly prayer time during your lunch break",
+				value: "",
+			},
+			{
+				title: "Sabbath",
+				body:
+					"God has created us to live in a rhythm of work and rest that embraces our personal limitations.  Which day of the week will you refrain from work and slow down to be with God in Sabbath rest as you delight, worship, and reflect?",
+				value: "",
+				placeholder:
+					"Choosing a 24 hour period, Limiting or Eliminating Screen-time, Spending time in worship, Making a meal as family/household, reading a novel/ poetry, Spending time in nature, etc.",
 			},
 		],
 	},
 	{
-		label: "Weekly Practices",
+		title: "Personal Practices",
+		description:
+			"People are complex and uniquely wired. As we learn to follow Jesus we must consider the various aspects of who we are (Mind, Body, and Heart) and how we are uniquely invited to journey towards wholeness in.",
 		practices: [
 			{
-				title:
-					"Looking to each week, which Sunday service (10am, 11am, 4.30pm) do you plan to connect with?",
-				label: "Come",
-				value: "",
-				placeholder: "Join 430pm on Sunday",
-			},
-			{
-				title:
-					"Which Hub do you want to belong to in order to build community?",
-				label: "Belong",
-				value: "",
-				placeholder: "Jonah and Abi’s hub every Thursday evening",
-			},
-			{
-				title:
-					"Almost there...what is the way you plan to join the rest of the church in serving the city each week?",
-				label: "Serve",
-				value: "",
-				placeholder: "Volunteer wth Bankuet 2x a week",
-			},
-			{
-				title:
-					"Finally, you may well already tithe monthly but how could you practice generosity with your finances to bless those in need each week?",
-				label: "Give",
+				title: "Mind",
+				body:
+					"Live a life of Prayer and communion with God: (1) Abide in God’s love and in his presence; (2) Listen for how God speaks with you and invites you to Obey; and (3) Contend for the needs and for the renewal of your community, your city and the world. How will you spend this portion of your day? ",
 				value: "",
 				placeholder:
-					"Tithe to KXC monthly and give to someone in need once a week",
+					"Type your answer here... e.g. Praying a Psalm, the Lord's Prayer, Breath Prayer, Listening Prayer, Intercession, etc.",
+			},
+			{
+				title: "Body",
+				body:
+					"We long to be people who have soaked in words of Scripture.  When will you read and meditate on the Bible in order to orient your mind and heart around the living story of God's goodness and faithfulness?",
+				placeholder:
+					"Type your answer here... e.g. Scripture reading, daily devotional, Lectio Divina, Bible Study, etc.",
+				value: "",
+			},
+			{
+				title: "Heart",
+				body:
+					"Live a life of Prayer and communion with God: (1) Abide in God’s love and in his presence; (2) Listen for how God speaks with you and invites you to Obey; and (3) Contend for the needs and for the renewal of your community, your city and the world. How will you spend this portion of your day? ",
+				value: "",
+				placeholder:
+					"Type your answer here... e.g. Praying a Psalm, the Lord's Prayer, Breath Prayer, Listening Prayer, Intercession, etc.",
 			},
 		],
 	},
