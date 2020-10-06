3516cabdf3bfec2d6df1f5ce8ff563b1761a78e5
Gil <gilbertjvirgo@gmail.com>
2020-10-06 18:01:18 +0100

    Removed "/start" route

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React from "react";
-import { Router, Switch, Route } from "react-router-dom";
-import ReactGA from 'react-ga';
-import { createBrowserHistory } from 'history';
-import Results from "./pages/Results";
-import Start from "./pages/Start";
-import { ThemeProvider } from "styled-components";
-import Review from "./pages/Review/Review";
+import { Route, Router, Switch } from "react-router-dom";
+
 import FontFaceObserver from "fontfaceobserver";
 import Intro from "./pages/Intro";
-import theme from "./theme";
-import fields from "./fields";
-import Spinner from "./components/Spinner";
 import Preview from "./pages/Preview";
+import React from "react";
+import ReactGA from "react-ga";
+import Results from "./pages/Results";
+import Review from "./pages/Review/Review";
+import Spinner from "./components/Spinner";
+import Start from "./pages/Start";
+import { ThemeProvider } from "styled-components";
+import { createBrowserHistory } from "history";
+import fields from "./data/fields";
+import theme from "./theme";
 
 const trackingId = "UA-165932948-1";
 ReactGA.initialize(trackingId); // initialize ReactGA with trackingId
 
 const history = createBrowserHistory();
-history.listen(location => {
-  ReactGA.set({ page: location.pathname }); // Update the user's current page
-  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+history.listen((location) => {
+	ReactGA.set({ page: location.pathname }); // Update the user's current page
+	ReactGA.pageview(location.pathname); // Record a pageview for the given page
 });
-
 
 export default function App() {
 	const [fontsLoaded, setFontsLoaded] = React.useState(false);
@@ -80,7 +80,7 @@ export default function App() {
 								/>
 							)}
 						/>
-						<Route
+						{/* <Route
 							path="/start"
 							render={() => (
 								<Start
@@ -88,7 +88,7 @@ export default function App() {
 									onResultsChange={handleResultsChange}
 								/>
 							)}
-						/>
+						/> */}
 						<Route
 							path="/preview"
 							render={() => <Preview image={image} />}
