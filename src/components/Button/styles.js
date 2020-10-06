9e11c69c0c6d4c7e2895f89f4e6989adfbb24b3f
Gil <gilbertjvirgo@gmail.com>
2020-10-06 18:00:55 +0100

    Added button hover state

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,21 +1,35 @@
 import styled from "styled-components";
 
-export const Wrapper = styled.button`
-	border: none;
-	appearance: none;
-	background-color: transparent;
-	border-bottom: 2px solid white;
-	outline: none;
-	padding: 0;
-	color: white;
-`;
-
 export const Text = styled.span`
 	font-size: 18px;
 	line-height: 30px;
 	font-family: "neue-haas-grotesk-display", sans-serif;
 	font-weight: 600;
 	margin-right: 10px;
+	will-change: margin-right;
+	transition: margin-right 0.3s;
+`;
+
+export const Wrapper = styled.button`
+	border: none;
+	appearance: none;
+	background-color: transparent;
+	border-bottom-style: solid;
+	border-width: 2px;
+	border-color: rgba(255, 255, 255, 0.5);
+	transition: border-color 0.3s;
+	will-change: border-color;
+	outline: none;
+	padding: 0;
+	color: white;
+
+	&:hover {
+		border-color: rgba(255, 255, 255, 1);
+
+		${Text} {
+			margin-right: 20px;
+		}
+	}
 `;
 
 export const Arrow = styled.img.attrs({
