08bca6c3cf1749b95b962413d3d621cf81fb5af0
Gil <gilbertjvirgo@gmail.com>
2020-10-07 17:57:25 +0100

    Added test value for fields

--- src/data/fields.js
+++ src/data/fields.js
@@ -6,7 +6,7 @@ const fields = [
 				title: "Prayer",
 				body:
 					"Live a life of Prayer and communion with God: (1) Abide in God’s love and in his presence; (2) Listen for how God speaks with you and invites you to Obey; and (3) Contend for the needs and for the renewal of your community, your city and the world. How will you spend this portion of your day? ",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 				placeholder:
 					"Type your answer here... e.g. Praying a Psalm, the Lord's Prayer, Breath Prayer, Listening Prayer, Intercession, etc.",
 			},
@@ -16,13 +16,13 @@ const fields = [
 					"We long to be people who have soaked in words of Scripture.  When will you read and meditate on the Bible in order to orient your mind and heart around the living story of God's goodness and faithfulness?",
 				placeholder:
 					"Type your answer here... e.g. Scripture reading, daily devotional, Lectio Divina, Bible Study, etc.",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 			},
 			{
 				title: "Worship",
 				body:
 					"How will you prioritize your day to Worship God with gratitude and remember the moments of God's nearness and blessing during your day?",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 				placeholder:
 					"Type your answer here... e.g. worship through singing, keep a gratitude journal, Prayer of Examen, choose a few of Wesley's Noticing Questions to reflect on each evening",
 			},
@@ -32,13 +32,13 @@ const fields = [
 					"We choose to make a covenantal commitment to a group of fellow Jesus followers and authentically share our lives with them in intimate Community. Who is your covenantal community (Digital Community, House church or others) and when do you meet with them to share life and grow together?",
 				placeholder:
 					"Type your answer here... e.g., Confession, Regular meals together, Share resources with each other, Prayer requests and prayer, etc",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 			},
 			{
 				title: "Service",
 				body:
 					"We look beyond our own life and seek to be the hands and feet of Jesus in the world through sacrificial living and love of others. How will you commit to a life of Service for your family, neighbors, coworkers and your city?",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 				placeholder:
 					"Type your answer here... e.g., Regular prayer walks; Sign up to serve at the rescue mission, Volunteer with our reach team; Invite neighbors over for a meal; Join alpha etc.",
 			},
@@ -48,13 +48,13 @@ const fields = [
 					"As a community, we long to embody the extravagant Generosity that we see in Jesus. We cultivate this in us through a commitment and willingness to give and to bless others. How will you do this in your life?",
 				placeholder:
 					"Type your answer here... e.g., Monthly tithe to your church community, supporting missionaries or an organization you believe in, practice tipping 30%, regularly make or buy meals for those in need",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 			},
 			{
 				title: "Steward",
 				body:
 					"We Steward our whole lives as a response to the grace and privilege God has given to each of us as his children. How will you be intentional with your  Time, Resources and Money and grow as a steward in God’s kingdom?",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 				placeholder:
 					"Type your answer here... e.g., Commit to living on a budget and within your means, take an audit of your time and reorder how you prioritize your time and energy",
 			},
@@ -64,13 +64,13 @@ const fields = [
 					"Your Vocation is a missional space to steward and practice living as the way of Jesus. Who are people in your workplace you can pray for? How will you grow in your vocation and commit your work towards God’s kingdom purposes?",
 				placeholder:
 					"Type your answer here... e.g., Make a list of coworkers and commit to praying for them; invite Jesus into your work schedule and responsibilities to help navigate your vocation; Commit to weekly prayer time during your lunch break",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 			},
 			{
 				title: "Sabbath",
 				body:
 					"God has created us to live in a rhythm of work and rest that embraces our personal limitations.  Which day of the week will you refrain from work and slow down to be with God in Sabbath rest as you delight, worship, and reflect?",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 				placeholder:
 					"Choosing a 24 hour period, Limiting or Eliminating Screen-time, Spending time in worship, Making a meal as family/household, reading a novel/ poetry, Spending time in nature, etc.",
 			},
@@ -85,7 +85,7 @@ const fields = [
 				title: "Mind",
 				body:
 					"Live a life of Prayer and communion with God: (1) Abide in God’s love and in his presence; (2) Listen for how God speaks with you and invites you to Obey; and (3) Contend for the needs and for the renewal of your community, your city and the world. How will you spend this portion of your day? ",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 				placeholder:
 					"Type your answer here... e.g. Praying a Psalm, the Lord's Prayer, Breath Prayer, Listening Prayer, Intercession, etc.",
 			},
@@ -95,13 +95,13 @@ const fields = [
 					"We long to be people who have soaked in words of Scripture.  When will you read and meditate on the Bible in order to orient your mind and heart around the living story of God's goodness and faithfulness?",
 				placeholder:
 					"Type your answer here... e.g. Scripture reading, daily devotional, Lectio Divina, Bible Study, etc.",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 			},
 			{
 				title: "Heart",
 				body:
 					"Live a life of Prayer and communion with God: (1) Abide in God’s love and in his presence; (2) Listen for how God speaks with you and invites you to Obey; and (3) Contend for the needs and for the renewal of your community, your city and the world. How will you spend this portion of your day? ",
-				value: "",
+				value: "Lorem ipsum dolor sit amet",
 				placeholder:
 					"Type your answer here... e.g. Praying a Psalm, the Lord's Prayer, Breath Prayer, Listening Prayer, Intercession, etc.",
 			},
