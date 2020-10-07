718c3c0574c97edd7ef2f9911f9e4a22c5b3aeac
Gil <gilbertjvirgo@gmail.com>
2020-10-07 17:57:35 +0100

    Added default themes file

---
+++ src/data/themes.js
@@ -0,0 +1,32 @@
+export default [
+	{
+		title: "Garden Church",
+		thumb: require("../assets/themes/gardenchurch-tn.jpg"),
+		image: require("../assets/themes/gardenchurch.jpg"),
+	},
+	{
+		title: "Ecru",
+		thumb: require("../assets/themes/ecru-tn.jpg"),
+		gradient: ["#FBF9F7", "#E2D1C3"],
+	},
+	{
+		title: "Dark",
+		thumb: require("../assets/themes/dark-tn.jpg"),
+		gradient: ["#434343", "#000000"],
+	},
+	{
+		title: "Night Time",
+		thumb: require("../assets/themes/nighttime-tn.jpg"),
+		gradient: ["#09203F", "#537895"],
+	},
+	{
+		title: "Sea Blue",
+		thumb: require("../assets/themes/seablue-tn.jpg"),
+		gradient: ["#00C6FB", "#005BEA"],
+	},
+	{
+		title: "Light",
+		thumb: require("../assets/themes/light-tn.jpg"),
+		gradient: ["#F5F7FA", "#C3CFE2"],
+	},
+];
