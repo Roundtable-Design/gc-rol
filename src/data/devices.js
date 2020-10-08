444c6c3d8f8f1337a279a6101a10ce0d650da279
Gil <gilbertjvirgo@gmail.com>
2020-10-08 17:49:24 +0100

    Further device info data added

--- src/data/devices.js
+++ src/data/devices.js
@@ -1,17 +1,41 @@
 export default [
 	{
-		title: "android",
-		image: require("../assets/devices/samsung-s20.svg"),
-		constraints: { width: 480, height: 800, dX: 9, dY: 15 },
+		title: "pixel-2",
+		icon: require("../assets/device-icons/pixel-2.svg"),
+		mockup: require("../assets/mockups/pixel-2.svg"),
+		constraints: {
+			dWidth: 420,
+			dHeight: 858,
+			sWidth: 375,
+			sHeight: 750,
+			dX: 21,
+			dY: 55,
+		},
 	},
 	{
 		title: "iphone-8",
-		image: require("../assets/devices/iphone-8.svg"),
-		constraints: { width: 375, height: 667, dX: 26, dY: 95 },
+		icon: require("../assets/device-icons/iphone-8.svg"),
+		mockup: require("../assets/mockups/iphone-8.svg"),
+		constraints: {
+			dWidth: 427,
+			dHeight: 858,
+			sWidth: 375,
+			sHeight: 667,
+			dX: 26,
+			dY: 95,
+		},
 	},
 	{
 		title: "iphone-11-pro",
-		image: require("../assets/devices/iphone-11-pro.svg"),
-		constraints: { width: 375, height: 812, dX: 26, dY: 23 },
+		icon: require("../assets/device-icons/iphone-11-pro.svg"),
+		mockup: require("../assets/mockups/iphone-11-pro.svg"),
+		constraints: {
+			dWidth: 427,
+			dHeight: 858,
+			sWidth: 375,
+			sHeight: 812,
+			dX: 26,
+			dY: 23,
+		},
 	},
 ];
