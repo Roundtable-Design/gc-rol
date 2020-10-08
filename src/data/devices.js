424a84a7fc7b635cef2d4c7c129d5b427e51bb01
Gil <gilbertjvirgo@gmail.com>
2020-10-08 10:48:03 +0100

    Added mockups and mockup placement data

--- src/data/devices.js
+++ src/data/devices.js
@@ -1,17 +1,17 @@
 export default [
 	{
 		title: "android",
-		image: require("../assets/devices/android.svg"),
-		constraints: [480, 800],
+		image: require("../assets/devices/samsung-s20.svg"),
+		constraints: { width: 480, height: 800, dX: 9, dY: 15 },
 	},
 	{
-		title: "iphone-6",
-		image: require("../assets/devices/iphone-6.svg"),
-		constraints: [375, 667],
+		title: "iphone-8",
+		image: require("../assets/devices/iphone-8.svg"),
+		constraints: { width: 375, height: 667, dX: 26, dY: 95 },
 	},
 	{
-		title: "iphone-x",
-		image: require("../assets/devices/iphone-x.svg"),
-		constraints: [375, 812],
+		title: "iphone-11-pro",
+		image: require("../assets/devices/iphone-11-pro.svg"),
+		constraints: { width: 375, height: 812, dX: 26, dY: 23 },
 	},
 ];
