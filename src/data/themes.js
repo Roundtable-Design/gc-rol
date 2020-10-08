377b5a68b42de6106b92a833c31aeed474b83816
Gil <gilbertjvirgo@gmail.com>
2020-10-08 17:49:45 +0100

    Themes file updated

--- src/data/themes.js
+++ src/data/themes.js
@@ -2,43 +2,39 @@ export default [
 	{
 		title: "Garden Church",
 		thumb: require("../assets/themes/gardenchurch-tn.jpg"),
-		textColor: "white",
 		gradient: require("../assets/themes/gardenchurch.jpg"),
 	},
 	{
 		title: "Ecru",
 		thumb: require("../assets/themes/ecru-tn.jpg"),
-		textColor: "black",
+		dark: true,
 		gradient: ["#FBF9F7", "#E2D1C3"],
 	},
 	{
 		title: "Dark",
 		thumb: require("../assets/themes/dark-tn.jpg"),
-		textColor: "white",
 		gradient: ["#434343", "#000000"],
 	},
 	{
 		title: "Night Time",
 		thumb: require("../assets/themes/nighttime-tn.jpg"),
-		textColor: "white",
 		gradient: ["#09203F", "#537895"],
 	},
 	{
 		title: "Sea Blue",
 		thumb: require("../assets/themes/seablue-tn.jpg"),
-		textColor: "white",
 		gradient: ["#00C6FB", "#005BEA"],
 	},
 	{
 		title: "Light",
 		thumb: require("../assets/themes/light-tn.jpg"),
-		textColor: "black",
+		dark: true,
 		gradient: ["#F5F7FA", "#C3CFE2"],
 	},
 	{
 		title: "Summer Daze",
 		thumb: require("../assets/themes/summerdaze-tn.jpg"),
-		textColor: "black",
+		dark: true,
 		gradient: [
 			"#F5F7FA",
 			"#B8CBB8",
@@ -51,7 +47,6 @@ export default [
 	{
 		title: "Colby",
 		thumb: require("../assets/themes/colby-tn.jpg"),
-		textColor: "white",
 		gradient: [
 			"#3F51B1",
 
