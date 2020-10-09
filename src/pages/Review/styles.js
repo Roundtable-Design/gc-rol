e33226a6e3f55122af65c26ebc4a18f817876c5b
Gil <gilbertjvirgo@gmail.com>
2020-10-09 11:05:25 +0100

    Fixed button positioning on Review screen

--- src/pages/Review/styles.js
+++ src/pages/Review/styles.js
@@ -1,16 +1,19 @@
-import styled from "styled-components";
 import { createBreakpoint } from "styled-components-breakpoint";
+import styled from "styled-components";
 import theme from "../../theme";
 
 const breakpoint = createBreakpoint(theme.breakpoints);
 
 export const ButtonWrapper = styled.div`
-	grid-column-start: 2;
-	grid-column-end: 6;
-
 	margin-bottom: 30px;
+	grid-column: 1 / -1;
+
+	${breakpoint("xs", "lg")`
+	display: flex;
+	justify-content: center;
+	`}
 
 	${breakpoint("lg")`
-        grid-column-start: 3;
+        grid-column: 3 / -1;
     `}
 `;
